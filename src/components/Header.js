e365efc8b44a518f2841348c4d031d5a92a17fe6
Gabriel Manara <gmanara@its.jnj.com>
2018-08-06 21:29:31 -0300

    feat: Add propType verification to Header component
    This commit consist in validate the prop that is passing to Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 export default function Header({title}) {
   return (
@@ -7,3 +8,7 @@ export default function Header({title}) {
     </div>
   )
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+};
