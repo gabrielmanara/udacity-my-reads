dd9af593761e9691e67ed8156c5d53d3f6a84216
Gabriel Manara <gmanara@its.jnj.com>
2018-08-09 19:58:28 -0300

    feat: Link between pages
    This commit consist in apply an link between initial and search page

--- src/views/BooksView.js
+++ src/views/BooksView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types'
 import BookList from '../components/BookList'
 import Header from "../components/Header";
@@ -44,9 +45,8 @@ class BooksView extends Component {
             </div>
           </div>
           <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>
-              Add a book
-            </a>
+            <Link to="/search">Add a book
+            </Link>
           </div>
         </div>
       </div>
